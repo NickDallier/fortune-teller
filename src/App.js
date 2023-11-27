6b1f9854ef8acd372fe12104dc9d7a0e1d9eeee8
Nick Dallier <nickdallier@gmail.com>
2023-11-26 20:06:38 -0500

    Added link to Fortune Generator

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ export default function App() {
             <ul>
                 <li><a href="http://nickdallier.ca#aboutMe">About Me</a></li>
                 <li><a href="http://nickdallier.ca#contact">Contact</a></li>
+                <li><a href="https://nickdallier-fortuneteller.netlify.app/">Fortune Generator</a></li>
                 <li><a href="https://nickdallier-jamming.netlify.app">Jammming</a></li>
             </ul>
         </nav>
